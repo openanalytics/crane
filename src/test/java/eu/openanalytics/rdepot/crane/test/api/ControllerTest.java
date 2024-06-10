ccd5b759f930b0a3df1c420cd92eb7767b11bd56
llionakis <luckianos@hotmail.com>
2024-06-10 17:02:37 +0200

    ref #32909: test index and logout page

--- src/test/java/eu/openanalytics/rdepot/crane/test/api/ControllerTest.java
+++ src/test/java/eu/openanalytics/rdepot/crane/test/api/ControllerTest.java
@@ -8,15 +8,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class ControllerTest {
     private static final CraneInstance inst = new CraneInstance("application-test-api.yml");
     private static final CraneInstance redisInst = new CraneInstance("application-test-redis.yml", 7071, new HashMap<>(), false);
 
     @AfterAll
-    public static void afterAll() { 
+    public static void afterAll() {
         inst.close();
-        redisInst.close(); 
+        redisInst.close();
     }
 
     @Test
@@ -49,6 +50,11 @@ public class ControllerTest {
         Response resp = apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest("/logout"));
         resp.assertSuccess();
         resp.assertRedirectedTo("/logout-success");
+
+
+        resp = apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest("/logout"));
+        resp.assertSuccess();
+        resp.assertRedirectedTo("/logout-success");
     }
 
     @Test
@@ -60,5 +66,33 @@ public class ControllerTest {
         resp = apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest("/logout"));
         resp.assertSuccess();
         resp.assertRedirectedTo("/logout-success");
+    }
+
+    @Test
+    public void indexPage() {
+        ApiTestHelper apiTestHelper = new ApiTestHelper(inst);
+
+        Response resp = apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest("/"));
+        String body = resp.body();
+        List<String> repositories = List.of("cache_txt_repo", "public_repo", "cache_txt_and_csv_repo", "mime_types");
+        Assertions.assertTrue(repositories.stream().allMatch(body::contains));
+
+        resp = apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest("/"));
+        body = resp.body();
+        repositories = List.of(
+            "public_repo", "cache_txt_and_csv_repo", "cache_txt_repo", "mime_types",
+            "restricted_expression_using_and_repo", "restricted_simple_expression_repo",
+            "restricted_to_groups_repo", "restricted_to_group_repo", "restricted_repo",
+            "private_repo", "restricted_to_users_repo", "restricted_repo"
+        );
+        Assertions.assertTrue(repositories.stream().allMatch(body::contains));
+
+        resp = apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest("/"));
+        body = resp.body();
+        repositories = List.of(
+            "private_repo", "public_repo", "cache_txt_and_csv_repo", "cache_txt_repo", "mime_types",
+            "restricted_simple_expression_repo", "restricted_to_groups_repo", "restricted_to_users_repo"
+        );
+        Assertions.assertTrue(repositories.stream().allMatch(body::contains));
     }
 }
