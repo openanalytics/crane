49091211cd223fc8e34f6c8e923d92a0da6e8b1a
llionakis <luckianos@hotmail.com>
2024-06-18 11:46:41 +0200

    ref #32837: use autowired instead of inject

--- src/main/java/eu/openanalytics/rdepot/crane/util/StartupEventListener.java
+++ src/main/java/eu/openanalytics/rdepot/crane/util/StartupEventListener.java
@@ -1,19 +1,38 @@
+/**
+ * Crane
+ *
+ * Copyright (C) 2021-2022 Open Analytics
+ *
+ * ===========================================================================
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Apache License as published by
+ * The Apache Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Apache License for more details.
+ *
+ * You should have received a copy of the Apache License
+ * along with this program.  If not, see <http://www.apache.org/licenses/>
+ */
 package eu.openanalytics.rdepot.crane.util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-import javax.inject.Inject;
-
 @Component
 public class StartupEventListener {
-    private final Logger LOGGER = LoggerFactory.getLogger(StartupEventListener.class);
+    private final Logger logger = LoggerFactory.getLogger(StartupEventListener.class);
 
-    @Inject
+    @Autowired
     private BuildProperties buildProperties;
 
     @EventListener
@@ -22,6 +41,6 @@ public class StartupEventListener {
             "Started %s %s",
             buildProperties.getName(), buildProperties.getVersion()
         );
-        LOGGER.info(startupMsg);
+        logger.info(startupMsg);
     }
 }
