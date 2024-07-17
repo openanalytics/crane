fda452e223acacce2b5017ab5b7c84a93cbd1956
llionakis <lucianos.lionakis@openanalytics.eu>
2024-07-17 17:48:56 +0200

    ref #33393: add tests for posix fs permissions

--- src/test/java/eu/openanalytics/rdepot/crane/test/service/PosixAccessControlServiceTest.java
+++ src/test/java/eu/openanalytics/rdepot/crane/test/service/PosixAccessControlServiceTest.java
@@ -20,7 +20,11 @@
  */
 package eu.openanalytics.rdepot.crane.test.service;
 
+import eu.openanalytics.rdepot.crane.test.helpers.ApiTestHelper;
 import eu.openanalytics.rdepot.crane.test.helpers.KeycloakInstance;
+import eu.openanalytics.rdepot.crane.test.helpers.Response;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -31,6 +35,7 @@ import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
 @Testcontainers
@@ -59,5 +64,202 @@ public class PosixAccessControlServiceTest {
         craneApp.withLogConsumer(new Slf4jLogConsumer(logger));
         craneApp.start();
     }
+    @AfterAll
+    public static void afterAll() {
+        craneApp.stop();
+        craneApp.close();
+    }
 
+    private final String craneUrl = String.format("http://%s:%s", craneApp.getHost(), cranePort);
+
+    @Test
+    public void testIndexPage() {
+        ApiTestHelper apiTestHelper = ApiTestHelper.from(craneUrl);
+
+        Response resp = apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest("/"));
+        String body = resp.body();
+        List<String> repositories = List.of("public_repo");
+        Assertions.assertTrue(repositories.stream().allMatch(body::contains));
+
+        resp = apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest("/"));
+        body = resp.body();
+        repositories = List.of("public_repo", "only_owner_demo", "only_group_scientists", "only_group_mathematicians");
+        Assertions.assertTrue(repositories.stream().allMatch(body::contains));
+
+        resp = apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest("/"));
+        body = resp.body();
+        repositories = List.of("public_repo", "only_group_mathematicians");
+        Assertions.assertTrue(repositories.stream().allMatch(body::contains));
+    }
+
+
+    @Test
+    public void testAccessToPublicRepository() {
+        ApiTestHelper apiTestHelper = ApiTestHelper.from(craneUrl);
+        String repository = "/public_repo";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(repository)).assertSuccess();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(repository)).assertSuccess();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(repository)).assertSuccess();
+
+
+        String file = repository + "/index.html";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(file)).assertSuccess();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(file)).assertSuccess();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(file)).assertSuccess();
+    }
+
+    @Test
+    public void testAccessToGroupRepositoryWithMultipleUsers() {
+        ApiTestHelper apiTestHelper = ApiTestHelper.from(craneUrl);
+        String repository = "/only_group_mathematicians";
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(repository)).assertSuccess();
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(repository)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(repository)).assertSuccess();
+
+        String file = repository + "/index.html";
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(file)).assertSuccess();
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(file)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(file)).assertSuccess();
+    }
+
+    @Test
+    public void testAccessToRepositoryWithOwnerRestrictionAccess() {
+        ApiTestHelper apiTestHelper = ApiTestHelper.from(craneUrl);
+        String repository = "/only_owner_demo";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(repository)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(repository)).assertSuccess();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(repository)).assertNotFound();
+
+        String file = repository + "/index.html";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(file)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(file)).assertSuccess();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(file)).assertNotFound();
+    }
+
+    @Test
+    public void testAccessToRepositoryRestrictedToGroupOfSingleUser() {
+        ApiTestHelper apiTestHelper = ApiTestHelper.from(craneUrl);
+        String repository = "/only_group_scientists";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(repository)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(repository)).assertSuccess();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(repository)).assertNotFound();
+
+        String file = repository + "/index.html";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(file)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(file)).assertSuccess();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(file)).assertNotFound();
+    }
+
+    @Test
+    public void testAccessToRepositoryWithNoAccessInPosix() {
+        ApiTestHelper apiTestHelper = ApiTestHelper.from(craneUrl);
+        String repository = "/no_posix_access";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(repository)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(repository)).assertNotFound();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(repository)).assertNotFound();
+
+        String file = repository + "/index.html";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(file)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(file)).assertNotFound();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(file)).assertNotFound();
+    }
+
+    @Test
+    public void testAccessToRepositoryWithNoPathAccess() {
+        ApiTestHelper apiTestHelper = ApiTestHelper.from(craneUrl);
+        String repository = "/restricted_to_groups_repo";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(repository)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(repository)).assertNotFound();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(repository)).assertNotFound();
+
+        String file = repository + "/index.html";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(file)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(file)).assertNotFound();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(file)).assertNotFound();
+    }
+
+    @Test
+    public void testAccessToNestedPublicRepository() {
+        ApiTestHelper apiTestHelper = ApiTestHelper.from(craneUrl);
+        String repository = "/public_repo/public_repo";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(repository)).assertSuccess();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(repository)).assertSuccess();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(repository)).assertSuccess();
+
+
+        String file = repository + "/index.html";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(file)).assertSuccess();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(file)).assertSuccess();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(file)).assertSuccess();
+    }
+
+    @Test
+    public void testAccessToNestedGroupRepositoryWithMultipleUsers() {
+        ApiTestHelper apiTestHelper = ApiTestHelper.from(craneUrl);
+        String repository = "/public_repo/only_group_mathematicians";
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(repository)).assertSuccess();
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(repository)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(repository)).assertSuccess();
+
+        String file = repository + "/index.html";
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(file)).assertSuccess();
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(file)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(file)).assertSuccess();
+    }
+
+    @Test
+    public void testAccessToNestedRepositoryWithOwnerRestrictionAccess() {
+        ApiTestHelper apiTestHelper = ApiTestHelper.from(craneUrl);
+        String repository = "/public_repo/only_owner_demo";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(repository)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(repository)).assertSuccess();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(repository)).assertNotFound();
+
+        String file = repository + "/index.html";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(file)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(file)).assertSuccess();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(file)).assertNotFound();
+    }
+
+    @Test
+    public void testAccessToNestedRepositoryRestrictedToGroupOfSingleUser() {
+        ApiTestHelper apiTestHelper = ApiTestHelper.from(craneUrl);
+        String repository = "/public_repo/only_group_scientists";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(repository)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(repository)).assertSuccess();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(repository)).assertNotFound();
+
+        String file = repository + "/index.html";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(file)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(file)).assertSuccess();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(file)).assertNotFound();
+    }
+
+    @Test
+    public void testAccessToNestedRepositoryWithNoAccessInPosix() {
+        ApiTestHelper apiTestHelper = ApiTestHelper.from(craneUrl);
+        String repository = "/public_repo/no_posix_access";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(repository)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(repository)).assertNotFound();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(repository)).assertNotFound();
+
+        String file = repository + "/index.html";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(file)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(file)).assertNotFound();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(file)).assertNotFound();
+    }
+
+    @Test
+    public void testAccessToNestedRepositoryWithNoPathAccess() {
+        ApiTestHelper apiTestHelper = ApiTestHelper.from(craneUrl);
+        String repository = "/public_repo/restricted_to_groups_repo";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(repository)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(repository)).assertNotFound();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(repository)).assertNotFound();
+
+        String file = repository + "/index.html";
+        apiTestHelper.callWithoutAuth(apiTestHelper.createHtmlRequest(file)).assertUnauthorizedRedirectToLogIn();
+        apiTestHelper.callWithAuth(apiTestHelper.createHtmlRequest(file)).assertNotFound();
+        apiTestHelper.callWithAuthTestUser(apiTestHelper.createHtmlRequest(file)).assertNotFound();
+    }
 }
