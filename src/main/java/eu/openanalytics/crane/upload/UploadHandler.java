73280fbf7899a27ca3e771eece1735d5b4a99092
llionakis <lucianos.lionakis@openanalytics.eu>
2024-09-05 10:08:06 +0200

    ref #33542: add s3 upload and handle multiple parts for local case

--- src/main/java/eu/openanalytics/crane/upload/UploadHandler.java
+++ src/main/java/eu/openanalytics/crane/upload/UploadHandler.java
@@ -1,44 +1,33 @@
 package eu.openanalytics.crane.upload;
 
-import eu.openanalytics.crane.config.CraneConfig;
-import eu.openanalytics.crane.model.config.Repository;
 import eu.openanalytics.crane.model.dto.ApiResponse;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.Part;
 import org.apache.commons.fileupload2.core.FileItemInput;
-import org.apache.commons.fileupload2.core.MultipartInput;
+import org.apache.commons.fileupload2.core.FileItemInputIterator;
 import org.apache.commons.fileupload2.jakarta.servlet6.JakartaServletFileUpload;
-import org.apache.commons.io.FileUtils;
 import org.carlspring.cloud.storage.s3fs.S3Path;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import software.amazon.awssdk.core.SdkClient;
-import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.core.async.AsyncRequestBody;
+import software.amazon.awssdk.core.async.BlockingInputStreamAsyncRequestBody;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.transfer.s3.S3TransferManager;
+import software.amazon.awssdk.transfer.s3.model.Upload;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Iterator;
+import java.nio.file.StandardOpenOption;
 import java.util.Map;
 
 @Controller
 public class UploadHandler {
-    private final CraneConfig config;
-
-    public UploadHandler(CraneConfig config) {
-        this.config = config;
-    }
+    private S3TransferManager transferManager;
+    private S3AsyncClient s3AsyncClient;
 
     @ResponseBody
     @PostMapping(value="/__upload", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -52,11 +41,13 @@ public class UploadHandler {
         JakartaServletFileUpload upload = new JakartaServletFileUpload();
 
         try {
-            for (Iterator<FileItemInput> it = upload.getItemIterator(request).asIterator(); it.hasNext(); ) {
-                FileItemInput item = it.next();
-                if (!item.isFormField()) {
-                    writeFile(item, request);
-                }
+            Path path = (Path) request.getAttribute("path");
+            if (path.toString().startsWith("s3://")) {
+                writeFileToS3(upload, request);
+            } else if (path.toString().startsWith("file://")) {
+                writeFileToLocalFileSystem(upload, request);
+            } else {
+                throw new RuntimeException("Path type no supported %s!".formatted(path.toString()));
             }
             return ApiResponse.success(Map.of("message", "File upload was successful"));
         } catch (IOException e) {
@@ -65,20 +56,50 @@ public class UploadHandler {
         }
     }
 
-    private void writeFile(FileItemInput item, HttpServletRequest request) throws IOException {
-        Path path = (Path) request.getAttribute("path");
-        if (path.toString().startsWith("s3://")) {
-            S3Client s3Client = S3Client.create();
-            S3Path s3Path = (S3Path) path;
-            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                    .bucket(s3Path.getBucketName())
-                    .key(s3Path.getKey()).build();
-            s3Client.putObject(putObjectRequest, RequestBody.fromString(new String(item.getInputStream().readAllBytes(), StandardCharsets.UTF_8)));
-            s3Client.close();
-        } else if (path.toString().startsWith("file://")) {
-            FileUtils.copyInputStreamToFile(item.getInputStream(), path.toFile());
-        } else {
-            throw new RuntimeException("Path type no supported %s".formatted(path.toString()));
+    private void writeFileToLocalFileSystem(JakartaServletFileUpload upload, HttpServletRequest request) throws IOException {
+        FileItemInputIterator fileItemInputIterator = upload.getItemIterator(request);
+        if (fileItemInputIterator.hasNext()) {
+            Path path = (Path) request.getAttribute("path");
+            while (fileItemInputIterator.hasNext()) {
+                FileItemInput fileItemInput = fileItemInputIterator.next();
+                Files.write(path, fileItemInput.getInputStream().readAllBytes(), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+            }
         }
+    }
+
+    private void writeFileToS3(JakartaServletFileUpload upload, HttpServletRequest request) throws IOException {
+        FileItemInputIterator fileItemInputIterator = upload.getItemIterator(request);
+        if (fileItemInputIterator.hasNext()) {
+            BlockingInputStreamAsyncRequestBody body =
+                    AsyncRequestBody.forBlockingInputStream(null); // 'null' indicates a stream will be provided later.
+
+            S3Path s3Path = (S3Path) request.getAttribute("path");
+            Upload s3UploadRequest = getTransferManager().upload(builder -> builder
+                    .requestBody(body)
+                    .putObjectRequest(req -> req.bucket(s3Path.getBucketName()).key(s3Path.getKey()))
+                    .build());
+
+            while (fileItemInputIterator.hasNext()) {
+                FileItemInput fileItemInput = fileItemInputIterator.next();
+                if (!fileItemInput.isFormField()) {
+                    body.writeInputStream(fileItemInput.getInputStream());
+                }
+            }
+            s3UploadRequest.completionFuture().join();
+        }
+    }
+
+    private S3TransferManager getTransferManager() {
+        if (transferManager == null) {
+            transferManager = S3TransferManager.builder().s3Client(getAsyncClient()).build();
+        }
+        return transferManager;
+    }
+
+    private S3AsyncClient getAsyncClient() {
+        if (s3AsyncClient == null) {
+            s3AsyncClient = S3AsyncClient.builder().multipartEnabled(true).build();
+        }
+        return s3AsyncClient;
     }
 }
