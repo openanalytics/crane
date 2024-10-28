82b2f9c078b5443b9c46a7c8fb71911ae9a81054
llionakis <lucianos.lionakis@openanalytics.eu>
2024-10-28 17:12:49 +0100

    ref #34010: add missing lisences and document why the converters are needed

--- src/main/java/eu/openanalytics/crane/model/PathComponentConverter.java
+++ src/main/java/eu/openanalytics/crane/model/PathComponentConverter.java
@@ -1,3 +1,23 @@
+/**
+ * Crane
+ *
+ * Copyright (C) 2021-2024 Open Analytics
+ *
+ * ===========================================================================
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Apache License as published by
+ * The Apache Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Apache License for more details.
+ *
+ * You should have received a copy of the Apache License
+ * along with this program.  If not, see <http://www.apache.org/licenses/>
+ */
 package eu.openanalytics.crane.model;
 
 import eu.openanalytics.crane.model.config.PathComponent;
@@ -6,9 +26,15 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesBindin
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
+/**
+ * The PathComponentConverter is required to parse paths that have no properties defined.
+ * PathComponents without properties in the application.yml are passed as empty Strings which can't be
+ * directly converted to a PathComponent.
+ */
 @Component
 @ConfigurationPropertiesBinding
 public class PathComponentConverter implements Converter<String, PathComponent> {
+
     @Override
     public PathComponent convert(@NotNull String source) {
         return new PathComponent();
--- src/main/java/eu/openanalytics/crane/model/RepositoryConverter.java
+++ src/main/java/eu/openanalytics/crane/model/RepositoryConverter.java
@@ -1,3 +1,23 @@
+/**
+ * Crane
+ *
+ * Copyright (C) 2021-2024 Open Analytics
+ *
+ * ===========================================================================
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Apache License as published by
+ * The Apache Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Apache License for more details.
+ *
+ * You should have received a copy of the Apache License
+ * along with this program.  If not, see <http://www.apache.org/licenses/>
+ */
 package eu.openanalytics.crane.model;
 
 import eu.openanalytics.crane.model.config.Repository;
@@ -9,6 +29,11 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationPropertiesBinding
 public class RepositoryConverter implements Converter<String, Repository> {
+    /**
+     * The RepositoryConverter is required to parse repositories that have no properties defined.
+     * Repositories without properties in the application.yml are passed as empty Strings which can't be
+     * directly converted to a Repository.
+     */
     @Override
     public Repository convert(@NotNull String source) {
         return new Repository();
